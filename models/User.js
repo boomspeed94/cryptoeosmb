582ee343015c89d7e4bac1eb5f8130b7343028d8
Truong Nguyen <truongnguyen@minhhop.net>
2019-01-15 11:38:21 +0700

    update

--- models/User.js
+++ models/User.js
@@ -13,17 +13,17 @@ var UserSchema = new mongoose.Schema({
 
 UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
 
-UserSchema.methods.validPassword = function(password){
+UserSchema.methods.validPassword = function(password) {
   var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
   return this.hash === hash;
 };
 
 UserSchema.methods.setPassword = function(password){
   this.salt = crypto.randomBytes(16).toString('hex');
-  this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
 };
 
-UserSchema.methods.generateJWT = function(password){
+UserSchema.methods.generateJWT = function() {
   var today = new Date();
   var exp = new Date(today);
   exp.setDate(today.getDate() + 60);
@@ -35,7 +35,7 @@ UserSchema.methods.generateJWT = function(password){
   }, secret);
 };
 
-UserSchema.methods.toAuthJSON = function(password){
+UserSchema.methods.toAuthJSON = function(){
   return {
     username: this.username,
     email: this.email,
